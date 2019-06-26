a3330b21c8cb5d1a248fa0435c67197cd737c1cc
Lab Laptop <futureoceanlab@gmail.com>
2019-06-25 23:58:19 -0400

    fromLL

--- HMC6343_test.py
+++ HMC6343_test.py
@@ -233,7 +233,7 @@ class HMC6343(object):
             sleep(self.TD_POST_DATA)
             # readValues = bus.read_byte(self.I2C_ADDR)
             print("LEN", self.BLEN_POST_DATA)
-            readValues = bus.read_bytes(self.I2C_ADDR, 2)
+            readValues = bus.read_bytes(self.I2C_ADDR, 3)
             print("readVals: ", readValues)
 
 
