48f8f9c5ee4f2b26c42cae7c9aa8e92b9589a734
Lab Laptop <futureoceanlab@gmail.com>
2019-06-25 22:26:39 -0400

    fromLL

--- HMC6343_test.py
+++ HMC6343_test.py
@@ -325,7 +325,7 @@ class HMC6343(object):
             sleep(self.TD_POST_DATA)
             readValues = bus.read_byte(self.I2C_ADDR)
             print("Value of OpMode1= 0x%02x" %readValues)
-            return readValues[0]
+            return readValues
 
 
         #old
